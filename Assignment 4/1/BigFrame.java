74d30a9fb0c40f88780e4b8b6e6b7d283c37f87b
Eversco <36009753+Eversco@users.noreply.github.com>
2018-10-03 13:11:56 -0700

    Assignment 4

--- Assignment 4/1/BigFrame.java
+++ Assignment 4/1/BigFrame.java
@@ -4,7 +4,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 
 public class BigFrame extends Application {
-    public static void main(String[] args) {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World");
 
+        Label HelloWorldLabel = new Label("Hello World");
+        Scene scene = new Scene(HelloWorldLabel,  400, 400);
+
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
 }
--- Assignment 4/2/SumSquaresInRange.java
+++ Assignment 4/2/SumSquaresInRange.java
@@ -1,5 +1,15 @@
 class SumSquaresInRange {
+    static int SumSquare(int x) {
+        if (x == 0) // If value given 0 then Return 0
+            return 0;
+        else
+            return (x * x) + SumSquare(x - 1); // variable square and decrease original variable and and do again 
+    }
+
+
     public static void main(String[] args) {
-        
+        int argument = 4; // given value
+        int result = SumSquare(argument); // value to calculator
+        System.out.println(result);// print out
     }
 }
